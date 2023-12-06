aa1d987b068aeedb09ea096b4aa067cf7fe28f8b
qortasiandrasteppe-pvcc <149604679+qortasiandrasteppe-pvcc@users.noreply.github.com>
2023-12-05 23:04:50 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Qortasiandra Steppe
+# Prog Purpose: Hello World
+
+print("Hello World! This is my first Python Program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("And lots of other things!")
